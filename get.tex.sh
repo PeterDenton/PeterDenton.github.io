72f3154a46463de627d5e52003a57bb118511877
jazzwhiz <peterbd1@gmail.com>
2014-03-15 13:12:09 -0500

    gets tex and pdf and converts tex to html

---
+++ get.tex.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+get_tex_as_html()
+{
+	cwd=$(pwd)
+	name=$(basename $1)
+	cd $name
+
+	cp $1/*.bst .
+	cp $1/$name.tex .
+	cp $1/*.bib .
+
+	htlatex $name.tex
+	for f in *.aux
+	do
+		bibtex $f
+	done
+	htlatex $name.tex
+	htlatex $name.tex
+
+	cp $1/$name.pdf .
+	
+	rm $name.4ct
+	rm $name.4tc
+	rm *.aux
+	rm *.bbl
+	rm *.bib
+	rm *.blg
+	rm *.bst
+	rm $name.dvi
+	rm $name.idv
+	rm $name.lg
+	rm $name.log
+	rm $name.tex
+	rm $name.tmp
+	rm $name.xht
+	rm $name.xref
+
+	cd $cwd
+}
+
+get_tex_as_html /home/peter/Dropbox/Documents/Essays/CV
+get_tex_as_html /home/peter/Dropbox/Documents/Essays/Publication_List
