8efa3a815628f17f132f1b73304cc562919d3475
jazzwhiz <peterbd1@gmail.com>
2014-03-17 13:59:31 -0500

    updated colors

---
+++ update.tex.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+tex_2_html()
+{
+	cwd=$(pwd)
+	name=$(basename $1)
+	cd $1
+
+	htlatex $name.tex
+
+	more $cwd/main.css >> $name.css
+
+	rm $name.4ct
+	rm $name.4tc
+	rm $name.idv
+	rm $name.lg
+	rm $name.tmp
+	rm $name.xht
+	rm $name.xref
+
+	cd $cwd
+}
+
+mod_css()
+{
+	cwd=$(pwd)
+	name=$(basename $1)
+	cd $1
+
+	more $cwd/main.css >> $name.css
+
+	cd $cwd
+}
+
+update()
+{
+#	tex_2_html $1
+	mod_css $1
+}
+
+update /home/peter/Dropbox/Documents/Essays/CV
+update /home/peter/Dropbox/Documents/Essays/Publication_List
