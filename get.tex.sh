3fb23a6b49d9c7a74209ba273d6676c0018be6fd
jazzwhiz <peterbd1@gmail.com>
2014-03-17 13:47:29 -0500

    now cv and pub list are maintained in their own repos

--- get.tex.sh
+++ get.tex.sh
@@ -4,36 +4,16 @@ get_tex_as_html()
 {
 	cwd=$(pwd)
 	name=$(basename $1)
-	cd $name
-
-	cp $1/*.bst .
-	cp $1/$name.tex .
-	cp $1/*.bib .
+	cd $1
 
 	htlatex $name.tex
-	for f in *.aux
-	do
-		bibtex $f
-	done
-	htlatex $name.tex
-	htlatex $name.tex
 
-	more ../main.css >> $name.css
+	more $cwd/main.css >> $name.css
 
-	cp $1/$name.pdf .
-	
 	rm $name.4ct
 	rm $name.4tc
-	rm *.aux
-	rm *.bbl
-	rm *.bib
-	rm *.blg
-	rm *.bst
-	rm $name.dvi
 	rm $name.idv
 	rm $name.lg
-	rm $name.log
-	rm $name.tex
 	rm $name.tmp
 	rm $name.xht
 	rm $name.xref
